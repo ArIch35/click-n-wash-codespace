953f7faa2c81c1d854582d4066808c6b7e38fcf1
aiRich <moses.antonio@stud.h-da.de>
2023-12-07 18:48:40 +0100

    Create the backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,28 @@
 import 'reflect-metadata';
+import express from "express";
+import userRouter from "./router/user.router";
+import cors from "cors";
+import { connectToDb } from './db';
 
-console.log('Hello world!');
+/**
+ * The main entry point for the application.
+ * This file is responsible for setting up the express server and the routes.
+ */
+
+connectToDb().then(() => {
+    console.log('Connected to DB');
+}).catch((err) => {
+    console.error("Failed to connect to DB", err);
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+
+app.use("/user", userRouter);
+
+app.listen(8080, () => {
+  console.log("Listening");
+});
+
