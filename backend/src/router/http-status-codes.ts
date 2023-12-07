da62e990255819dd5c7fa272abb6d38cc2f7bbef
aiRich <moses.antonio@stud.h-da.de>
2023-12-07 18:47:57 +0100

    Added status codes for http calls

---
+++ backend/src/router/http-status-codes.ts
@@ -0,0 +1,8 @@
+export const STATUS_OK: number = 200;
+export const STATUS_CREATED: number = 201;
+export const STATUS_BAD_REQUEST: number = 400;
+export const STATUS_UNAUTHORIZED: number = 401;
+export const STATUS_NOT_FOUND: number = 404;
+export const STATUS_SERVER_ERROR: number = 500;
+export const STATUS_CONFLICT: number = 409;
+export const STATUS_UNPROCESSABLE_ENTITY: number = 422;
