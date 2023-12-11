bd64047f665625d93f9bc1aa21ed491945e1a315
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-12-11 01:45:30 +0100

    Add before insert and update hooks to check for duplicate of
    laundromat name, given by other users and laundromat address

--- backend/src/entities/laundromat.ts
+++ backend/src/entities/laundromat.ts
@@ -1,5 +1,7 @@
-import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, Not, OneToMany } from 'typeorm';
 import { number, object, string } from 'yup';
+import getDb from '../db';
+import StatusError from '../utils/error-with-status';
 import BaseEntity from './base-entity';
 import User from './user';
 import WashingMachine from './washing-machine';
@@ -26,9 +28,59 @@ class Laundromat extends BaseEntity {
 
   @OneToMany(() => WashingMachine, (washingMachine) => washingMachine.laundromat)
   washingMachines?: WashingMachine[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  /**
+   * Rules to check before inserting or updating a laundromat.
+   */
+  async rules(): Promise<void> {
+    await checkDuplicateNameFromOtherUsers(this);
+    await checkDuplicateAddress(this);
+  }
 }
 
 export default Laundromat;
+
+/**
+ * Checks whether a laundromat with the same name and from other users exists in the database.
+ * @param laundromat The laundromat to check.
+ */
+const checkDuplicateNameFromOtherUsers = async (laundromat: Laundromat): Promise<void> => {
+  const existingLaundromat = await getDb().laundromatRepository.findOne({
+    where: {
+      name: laundromat.name,
+      owner: {
+        id: Not(laundromat.owner.id),
+      },
+    },
+  });
+  if (existingLaundromat) {
+    throw new StatusError(`${laundromat.name} has already been taken by another user`, 409);
+  }
+};
+
+/**
+ * Checks whether a laundromat with the same address exists in the database.
+ * @param laundromat The laundromat to check.
+ */
+const checkDuplicateAddress = async (laundromat: Laundromat): Promise<void> => {
+  const existingLaundromat = await getDb().laundromatRepository.findOne({
+    where: {
+      id: Not(laundromat.id),
+      street: laundromat.street,
+      city: laundromat.city,
+      country: laundromat.country,
+      postalCode: laundromat.postalCode,
+    },
+  });
+  if (existingLaundromat) {
+    throw new StatusError(
+      `Laundromat with address ${laundromat.street}, ${laundromat.city}, ${laundromat.country}, ${laundromat.postalCode} already exists`,
+      409,
+    );
+  }
+};
 
 export const createLaundromatSchema = object({
   name: string().required(),
