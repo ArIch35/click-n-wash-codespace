be73dd12542218dfb6ca1740a150dcf352239285
bernhard <Bernhard.R.Putranto@stud.h-da.de>
2023-12-12 15:59:04 +0100

    Add base layout

---
+++ frontend/src/layout/BaseLayout.tsx
@@ -0,0 +1,63 @@
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Overlay, Transition } from '@mantine/core';
+import { ReactNode, useState } from 'react';
+import Header from './Header/Header';
+import Navbar from '../components/navbar/Navbar';
+
+const BaseLayout = ({children} : {children: ReactNode}) => {
+  const [opened, { open, close }] = useDisclosure();
+  const [visible, setVisible] = useState(false);
+
+  const scaleX = {
+   in: { opacity: 1, transform: 'scaleX(1)' },
+   out: { opacity: 0, transform: 'scaleX(0)' },
+   common: { transformOrigin: 'left' },
+   transitionProperty: 'transform, opacity',
+ };
+ 
+  return (
+    <AppShell>
+     <AppShell.Header zIndex={0}>
+      <Header toggle={open} setVisible={setVisible} />
+     </AppShell.Header>
+
+     <AppShell.Main>
+      <div style={{position: 'absolute', zIndex:10}}>
+       <Transition
+        mounted={opened}
+        transition={scaleX}
+        timingFunction="ease"
+        keepMounted
+        duration={200}
+       >
+        {(transitionStyle) => (
+         <div style={transitionStyle}>
+          <Navbar />
+         </div>
+        )}
+       </Transition>
+      </div>
+      
+      <div 
+       style={{paddingTop: '45px', height:'100vh', zIndex: 0}} 
+       >
+        {children}
+        {visible && (
+          <Overlay
+           zIndex={1}
+           opacity={0.4}
+           blur={1}
+           onClick={(event) => {
+            event.preventDefault();
+            close();
+            setVisible(false);
+           }}
+          />
+        )}      
+       </div>
+     </AppShell.Main>
+    </AppShell>
+  );
+}
+
+export default BaseLayout;
