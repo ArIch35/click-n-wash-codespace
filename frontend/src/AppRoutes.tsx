1d5b6ca07346cc8faf2910289f1c6783d2984dbb
bernhard <Bernhard.R.Putranto@stud.h-da.de>
2023-12-12 15:58:17 +0100

    Implement routing

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,26 @@
+import {
+ Route,
+ RouteProps,
+ Routes,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+export const routes: RouteProps[] = [
+ {
+  path: '/',
+  element: <HomePage />,
+  index: true,
+ },
+];
+
+const AppRoutes = () => {
+ return(
+  <Routes>
+   {routes.map((route, index) => {
+    return <Route key={index} {...route} />;
+   })}
+  </Routes>
+ );
+}
+
+export default AppRoutes;
