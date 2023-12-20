050bab6323cb3ce46ae6b0abe18fe82f6edac30f
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-12-20 02:23:00 +0100

    Refactor how ENVs are loaded

--- frontend/src/providers/Notification.provider.tsx
+++ frontend/src/providers/Notification.provider.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Socket, io } from 'socket.io-client';
 import { RootState } from '../reducers/root.reducer';
+import loadEnv from '../utils/load-env';
 export const NotificationContext = createContext<null>(null);
 
 interface NotificationProviderProps {
@@ -10,7 +11,7 @@ interface NotificationProviderProps {
 
 const NotificationProvider = ({ children }: NotificationProviderProps) => {
   const userIdFromReducer = useSelector((state: RootState) => state.notificationState.userId);
-  const socket: Socket = io(import.meta.env.VITE_SERVER_ADDRESS as string);
+  const socket: Socket = io(loadEnv().VITE_SERVER_ADDRESS);
 
   useEffect(() => {
     if (userIdFromReducer) {
