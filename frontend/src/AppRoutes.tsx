ba0e8a167d1eba672eab6f3dca8c444e245ba590
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-12-28 18:42:49 +0100

    Refactor AppRoutes to add authentication check

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { RootState } from './reducers/root.reducer';
 import { routes } from './routeConstants';
+
+interface AuthRequiredProps {
+  children: React.ReactNode;
+  to?: string;
+}
+
+const AuthRequired = ({ children, to = '/' }: AuthRequiredProps) => {
+  const auth = useSelector((state: RootState) => state.authenticationState.firebaseData);
+  const { pathname } = useLocation();
+  if (!auth && pathname !== to) {
+    return <Navigate to={to} replace />;
+  }
+
+  return <>{children}</>;
+};
 
 const AppRoutes = () => {
   return (
     <Routes>
       {routes.map((route, index) => {
-        return <Route key={index} {...route} />;
+        const element = route.requireAuth ? (
+          <AuthRequired>{route.element}</AuthRequired>
+        ) : (
+          route.element
+        );
+        return <Route key={index} path={route.path} element={element} />;
       })}
     </Routes>
   );
