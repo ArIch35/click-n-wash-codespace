69c6bb24d745197305e92ffa46177fd9dcdb4e05
Moses Antonio <142450370+ArIch35@users.noreply.github.com>
2024-01-06 18:52:39 +0000

    Added Simple booking page without ui that will show all available laundromats from database

--- frontend/src/pages/BookingsPage.tsx
+++ frontend/src/pages/BookingsPage.tsx
@@ -1,7 +1,45 @@
-import { Text } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import Laundromat from '../interfaces/entities/laundromat';
+import { getLaundromats, getWashingMachinesByLaundromatId } from '../utils/api-functions';
+import BaseList from '../components/ui/BaseList.component';
+import IndividualLaundromat from '../components/ui/IndividualLaundromat.component';
+import WashingMachine from '../interfaces/entities/washing-machine';
 
 const BookingsPage = () => {
-  return <Text>Bookings page</Text>;
+  const [allLaundromats, setAllLaundromats] = useState<Laundromat[]>([]);
+
+  const getWM = (id: string) => {
+    getWashingMachinesByLaundromatId(id)
+      .then((laundromat: Laundromat) => {
+        const washingMachines: WashingMachine[] | undefined = laundromat.washingMachines;
+        console.log(washingMachines);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    getLaundromats()
+      .then((laundromats) => {
+        setAllLaundromats(laundromats);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    allLaundromats && (
+      <BaseList
+        items={allLaundromats}
+        IndividualComponent={IndividualLaundromat}
+        onItemClick={(item: Laundromat) => {
+          getWM(item.id);
+        }}
+      />
+    )
+  );
 };
 
 export default BookingsPage;
