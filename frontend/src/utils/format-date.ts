90f553ee3d5cf4867a95fa4f7e037189e4972412
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-01-08 01:12:29 +0100

    Add BookingsPage, and utils function formatDate and required fetch functions

---
+++ frontend/src/utils/format-date.ts
@@ -0,0 +1,17 @@
+/**
+ * Format date to string (dd.mm.yyyy hh:mm)
+ * @param date Date to format
+ * @returns Formatted date
+ */
+const formatDate = (date: Date): string => {
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear();
+
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  return `${day}.${month}.${year} ${hour}:${minutes}`;
+};
+
+export default formatDate;
