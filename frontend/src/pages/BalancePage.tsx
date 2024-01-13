0e0acd7483b88f9829a34fa939281bec0d34b025
stvitanh <vinsky.s.tanhansen@stud.h-da.de>
2024-01-13 18:24:31 +0100

    add buttons to add fund

--- frontend/src/pages/BalancePage.tsx
+++ frontend/src/pages/BalancePage.tsx
@@ -1,43 +1,57 @@
-import { Box, Center, Table, Text } from '@mantine/core';
+import { Button, Flex, Space, Table, Text } from '@mantine/core';
 
 const BalancePage = () => {
+  const ths = (
+    <Table.Tr>
+      <Table.Th fz="lg">Date</Table.Th>
+      <Table.Th fz="lg">Transaction Type</Table.Th>
+      <Table.Th fz="lg">Transcation Amount</Table.Th>
+      <Table.Th fz="lg">Balance</Table.Th>
+    </Table.Tr>
+  );
+
   return (
-    <Box mt="lg">
-      <Box m="xl">
-        <Text size="xl" fw={700}>
-          Balance Page
+    <Flex mt="lg" gap={50} direction="column">
+      <Flex gap="xl" justify={'space-between'} px="md">
+        <Text fz={35} fw={700}>
+          Balance History
         </Text>
-        <Text>Current Balance: $1000</Text>
-      </Box>
-      <Center>
-        <Table variant="simple">
-          <Table.Thead>
-            <Table.Tr>
-              <Table.Th w={1 / 3}>Date</Table.Th>
-              <Table.Th w={1 / 3}>Transaction</Table.Th>
-              <Table.Th w={1 / 3}>Balance</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
+        <Space />
+        <Text fz={35} fw={700}>
+          Current Balance: $2500
+        </Text>
+      </Flex>
+      <Flex mt="lg" px="md">
+        <Table fz="md" verticalSpacing="md" horizontalSpacing="md" withTableBorder highlightOnHover>
+          <Table.Thead>{ths}</Table.Thead>
           <Table.Tbody>
             <Table.Tr>
               <Table.Td>2021-06-01</Table.Td>
               <Table.Td>Deposit</Table.Td>
+              <Table.Td>$1000</Table.Td>
               <Table.Td>$1000</Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Td>2021-06-02</Table.Td>
               <Table.Td>Withdraw</Table.Td>
               <Table.Td>$500</Table.Td>
+              <Table.Td>$500</Table.Td>
             </Table.Tr>
             <Table.Tr>
               <Table.Td>2021-06-03</Table.Td>
               <Table.Td>Deposit</Table.Td>
               <Table.Td>$2000</Table.Td>
+              <Table.Td>$2500</Table.Td>
             </Table.Tr>
           </Table.Tbody>
         </Table>
-      </Center>
-    </Box>
+      </Flex>
+      <Flex mt="lg" justify={'flex-end'} px="md">
+        <Button radius={'100'} size="md">
+          Add Funds
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
