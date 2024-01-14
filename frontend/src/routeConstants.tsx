85870c31bcd96d43ec25c972306e5ff4be6e1d96
Vinston Salim <vinston.salim@stud.h-da.de>
2024-01-15 00:36:30 +0100

    Add Laundromat Details page to routeConstants.tsx

--- frontend/src/routeConstants.tsx
+++ frontend/src/routeConstants.tsx
@@ -15,6 +15,7 @@ import ManageLaundromatsPage from './pages/ManageLaundromatsPage';
 import MissingPage from './pages/MissingPage';
 import SettingsPage from './pages/SettingsPage';
 import AddLaundromatPage from './pages/AddLaundromatPage';
+import LaundromatDetailsPage from './pages/LaundromatDetailsPage';
 
 type Route = RouteProps & {
   label: string;
@@ -79,5 +80,13 @@ export const routes: Route[] = [
     element: <MissingPage />,
     label: 'Missing',
     icon: IconFileUnknown,
+  },
+  {
+    path: '/edit-laundromat/:id',
+    element: <LaundromatDetailsPage />,
+    label: 'Laundromat Details',
+    icon: IconBuildingStore,
+    requireAuth: true,
+    requireVendor: true,
   },
 ];
