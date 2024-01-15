62c2186f0df83568ce9afd6f990977621f1c12c4
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-01-15 19:22:39 +0100

    Add navigation functionality and display inbox messages

--- frontend/src/layout/Header/Header.tsx
+++ frontend/src/layout/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Burger, Button, Group } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import DarkModeToggle from '../../components/DarkModeToggle';
 import UserMenu from '../../components/UserMenu';
 import { AuthenticationForm } from '../../components/auth/AuthentificationForm';
@@ -11,44 +12,35 @@ import classes from './Header.module.css';
 
 const Header = ({ toggle, setVisible }: NavbarControllerProps) => {
   const user = useSelector((state: RootState) => state.authenticationState.user);
+  const navigate = useNavigate();
   const [modalOpened, modalHandlers] = useDisclosure(false);
 
   const loggedIn = React.useMemo(() => user, [user]);
 
   return (
     <header className={classes.header}>
-      <div className={classes.inner}>
-        {!loggedIn && <AuthenticationForm opened={modalOpened} onClose={modalHandlers.close} />}
-        <Group>
-          <Burger
-            onClick={() => {
-              if (!loggedIn) {
-                modalHandlers.open();
-                return;
-              }
-              toggle();
-              setVisible(true);
-            }}
-            size={'sm'}
-          />
-          <Button
-            size="md"
-            variant="transparent"
-            fw={700}
-            onClick={() => {
-              window.location.href = '/';
-            }}
-          >
-            Click n&apos; Wash
-          </Button>
+      {!loggedIn && <AuthenticationForm opened={modalOpened} onClose={modalHandlers.close} />}
+      <Group>
+        <Burger
+          onClick={() => {
+            if (!loggedIn) {
+              modalHandlers.open();
+              return;
+            }
+            toggle();
+            setVisible(true);
+          }}
+        />
+        <Button size="md" variant="transparent" fw={700} onClick={() => navigate('/')}>
+          Click n&apos; Wash
+        </Button>
+      </Group>
+      <Group>
+        <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
+          <UserMenu open={modalHandlers.open} />
+          <DarkModeToggle />
         </Group>
-        <Group>
-          <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
-            <UserMenu open={modalHandlers.open} />
-            <DarkModeToggle />
-          </Group>
-        </Group>
-      </div>
+      </Group>
     </header>
   );
 };
--- frontend/src/pages/InboxPage.tsx
+++ frontend/src/pages/InboxPage.tsx
@@ -1,8 +1,56 @@
+import { Stack, Table } from '@mantine/core';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '../reducers/authentication.reducer';
+import { RootState } from '../reducers/root.reducer';
+import { getUser } from '../utils/api-functions';
+
 const InboxPage = () => {
+  const user = useSelector((state: RootState) => state.authenticationState.user);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (!user) {
+      return;
+    }
+
+    // Refresh the user object from the server once
+    getUser(user.id)
+      .then((user) => {
+        dispatch(setUser(user));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!user) {
+    return null;
+  }
+
+  const rows = user.inbox?.map((message) => (
+    <Table.Tr key={message.id}>
+      <Table.Td>{message.name}</Table.Td>
+      <Table.Td>{message.content}</Table.Td>
+    </Table.Tr>
+  ));
+
+  const ths = (
+    <Table.Tr>
+      <Table.Th>Name</Table.Th>
+      <Table.Th>Content</Table.Th>
+    </Table.Tr>
+  );
+
   return (
-    <div>
-      <h1>Inbox</h1>
-    </div>
+    <Stack justify="center" align="center" p={'2rem'}>
+      <Table>
+        <Table.Thead>{ths}</Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </Stack>
   );
 };
 
