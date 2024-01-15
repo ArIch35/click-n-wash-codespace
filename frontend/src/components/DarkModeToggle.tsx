33a512bd64582cd0b6b3faa5538030b9a852e554
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-01-15 19:20:34 +0100

    Update DarkModeToggle component with icon size constants

--- frontend/src/components/DarkModeToggle.tsx
+++ frontend/src/components/DarkModeToggle.tsx
@@ -1,5 +1,6 @@
 import { ActionIcon, MantineColorScheme, Menu, useMantineColorScheme } from '@mantine/core';
 import { IconDeviceLaptop, IconMoon, IconSun } from '@tabler/icons-react';
+import { sizes } from '../utils/constants';
 
 interface Item {
   label: string;
@@ -8,18 +9,22 @@ interface Item {
 }
 
 const items: Item[] = [
-  { label: 'Light mode', value: 'light', icon: <IconSun /> },
-  { label: 'Dark mode', value: 'dark', icon: <IconMoon /> },
-  { label: 'System preferences', value: 'auto', icon: <IconDeviceLaptop /> },
+  { label: 'Light mode', value: 'light', icon: <IconSun size={sizes.iconSizeSmall} /> },
+  { label: 'Dark mode', value: 'dark', icon: <IconMoon size={sizes.iconSizeSmall} /> },
+  {
+    label: 'System preferences',
+    value: 'auto',
+    icon: <IconDeviceLaptop size={sizes.iconSizeSmall} />,
+  },
 ];
 
 const DarkModeToggle = () => {
   const { colorScheme, setColorScheme } = useMantineColorScheme();
 
   return (
-    <Menu shadow="md" width={200}>
+    <Menu trigger="hover" shadow="md" width={200}>
       <Menu.Target>
-        <ActionIcon variant="outline">
+        <ActionIcon size={sizes.iconSizeLarge} variant="outline">
           {items.find((item) => item.value === colorScheme)?.icon}
         </ActionIcon>
       </Menu.Target>
