b80200590c637bdd52d97f49ab17427511103316
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-01-15 19:41:30 +0100

    Refactor InboxPage component to mark all unread messages as read

--- frontend/src/pages/InboxPage.tsx
+++ frontend/src/pages/InboxPage.tsx
@@ -1,30 +1,24 @@
 import { Stack, Table } from '@mantine/core';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setUser } from '../reducers/authentication.reducer';
+import { useSelector } from 'react-redux';
 import { RootState } from '../reducers/root.reducer';
-import { getUser } from '../utils/api-functions';
+import { markAsRead } from '../utils/api-functions';
 
 const InboxPage = () => {
   const user = useSelector((state: RootState) => state.authenticationState.user);
-  const dispatch = useDispatch();
 
   React.useEffect(() => {
     if (!user) {
       return;
     }
 
-    // Refresh the user object from the server once
-    getUser(user.id)
-      .then((user) => {
-        dispatch(setUser(user));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    // Read all unread messages
+    markAsRead(
+      user.inbox?.filter((message) => !message.read).map((message) => message.id) || [],
+    ).catch((error) => {
+      console.log(error);
+    });
+  }, [user]);
 
   if (!user) {
     return null;
