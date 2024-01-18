85c87100db7420cdbb5d50be422ca87390314197
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-01-18 02:17:24 +0100

    Add inbox mode selection and separate user and vendor messages in InboxPage

--- frontend/src/pages/InboxPage.tsx
+++ frontend/src/pages/InboxPage.tsx
@@ -1,49 +1,94 @@
-import { Stack, Table } from '@mantine/core';
+import { Group, SegmentedControl, Stack, Table, Title } from '@mantine/core';
+import { useLocalStorage } from '@mantine/hooks';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../reducers/root.reducer';
+import { useAuth } from '../providers/authentication/Authentication.Context';
 import { markAsRead } from '../utils/api';
+import formatDate from '../utils/format-date';
 
 const InboxPage = () => {
-  const user = useSelector((state: RootState) => state.authenticationState.user);
+  const { user } = useAuth();
+  const [inboxMode, setInboxMode] = useLocalStorage<string>({
+    key: 'inboxMode',
+    defaultValue: 'user',
+  });
 
   React.useEffect(() => {
     if (!user) {
       return;
     }
 
-    // Read all unread messages
+    if (!user.isAlsoVendor) {
+      setInboxMode('user');
+    }
+
+    // Read all unread messages in the inbox
     markAsRead(
-      user.inbox?.filter((message) => !message.read).map((message) => message.id) || [],
+      user.inbox
+        ?.filter(
+          (message) =>
+            !message.read && (inboxMode === 'user' ? !message.forVendor : message.forVendor),
+        )
+        .map((message) => message.id) || [],
     ).catch((error) => {
       console.log(error);
     });
-  }, [user]);
+  }, [inboxMode, setInboxMode, user]);
 
   if (!user) {
     return null;
   }
 
-  const rows = user.inbox?.map((message) => (
-    <Table.Tr key={message.id}>
-      <Table.Td>{message.name}</Table.Td>
-      <Table.Td>{message.content}</Table.Td>
-    </Table.Tr>
-  ));
+  const userRows = user.inbox
+    ?.filter((message) => !message.forVendor)
+    .map((message) => (
+      <Table.Tr key={message.id}>
+        <Table.Td>{message.name}</Table.Td>
+        <Table.Td>{formatDate(message.createdAt)}</Table.Td>
+        <Table.Td>{message.content}</Table.Td>
+      </Table.Tr>
+    ));
+
+  const vendorRows = user.inbox
+    ?.filter((message) => message.forVendor)
+    .map((message) => (
+      <Table.Tr key={message.id}>
+        <Table.Td>{message.name}</Table.Td>
+        <Table.Td>{formatDate(message.createdAt)}</Table.Td>
+        <Table.Td>{message.content}</Table.Td>
+      </Table.Tr>
+    ));
 
   const ths = (
     <Table.Tr>
-      <Table.Th>Name</Table.Th>
-      <Table.Th>Content</Table.Th>
+      <Table.Th>NAME</Table.Th>
+      <Table.Th>RECEIVED</Table.Th>
+      <Table.Th>CONTENT</Table.Th>
     </Table.Tr>
   );
 
   return (
-    <Stack justify="center" align="center" p={'2rem'}>
-      <Table>
-        <Table.Thead>{ths}</Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
+    <Stack p="2rem">
+      <Group justify="flex-end">
+        {user.isAlsoVendor && (
+          <SegmentedControl
+            size="md"
+            radius="xs"
+            value={inboxMode}
+            onChange={setInboxMode}
+            data={[
+              { value: 'user', label: 'User inbox' },
+              { value: 'vendor', label: 'Vendor inbox' },
+            ]}
+          />
+        )}
+      </Group>
+      <Stack align="center" px="10%">
+        <Title order={1}>Inbox</Title>
+        <Table>
+          <Table.Thead>{ths}</Table.Thead>
+          <Table.Tbody>{inboxMode === 'user' ? userRows : vendorRows}</Table.Tbody>
+        </Table>
+      </Stack>
     </Stack>
   );
 };
