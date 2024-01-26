9e76204ab1a8d9b4adf6a145c562322502ad6a32
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-01-26 20:06:25 +0100

    Add getPositionFromAddress function to API utils

--- frontend/src/utils/api/index.ts
+++ frontend/src/utils/api/index.ts
@@ -1,4 +1,5 @@
 import firebaseAuth from '../../firebase';
+import { GetLocationLaundromat } from '../../interfaces/entities/laundromat';
 
 export interface Message {
   success: boolean;
@@ -14,6 +15,31 @@ export const headers = async () => {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${token}`,
   };
+};
+
+/**
+ * Retrieves the latitude and longitude coordinates of a given address.
+ * @param location - The location object containing the address details.
+ * @returns A promise that resolves to an object with the latitude and longitude coordinates.
+ * @throws An error if the API request fails or returns an error message.
+ */
+export const getPositionFromAddress = async (location: GetLocationLaundromat) => {
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/search?street=${location.street}&city=${location.city}&country=${location.country}&postalcode=${location.postalCode}&format=json&limit=1`,
+    {
+      headers: { ...(await headers()) },
+    },
+  );
+
+  const data = (await response.json()) as unknown;
+  if (!response.ok) {
+    throw new Error((data as Message).message);
+  }
+  if (!Array.isArray(data) || data.length === 0) {
+    throw new Error('No data found');
+  }
+
+  return data[0] as { lat: string; lon: string };
 };
 
 export * from './api-balance-transactions';
