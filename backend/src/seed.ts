9cedd1aa0f4e8d9a21435bd7ff01a091758f9ddd
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-01-31 19:35:11 +0100

    Update seeding

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -3,8 +3,8 @@ import getDb, { connectToDb } from './db';
 import Laundromat from './entities/laundromat';
 import User from './entities/user';
 import WashingMachine from './entities/washing-machine';
-import admin from './firebase-admin';
 import './utils/load-env';
+import { randomNumber } from './utils/utils';
 
 /**
  * Seed the database with random data
@@ -17,56 +17,74 @@ const seed = async () => {
 
   console.log('Seeding database...');
 
-  const totalUser = 5;
-  const totalLaundromat = 3;
-  const totalWashingMachine = 5;
+  const batchSize = 1000;
 
-  // Make 5 random users
+  const totalUser = randomNumber(100, 200);
   const users: User[] = [];
   for (let i = 0; i < totalUser; i++) {
-    const firebaseUser = await admin.auth().createUser({
-      email: fakerDE.internet.email(),
-      password: fakerDE.internet.password(),
-    });
     const user = getDb().userRepository.create({
-      id: firebaseUser.uid,
-      email: firebaseUser.email,
+      email: fakerDE.internet.email(),
       name: fakerDE.person.fullName(),
     });
     users.push(user);
   }
-  await getDb().userRepository.save(users);
 
-  // Make 3 random laundromats
+  console.log(`Seeding ${totalUser} users...`);
+  for (let i = 0; i < totalUser; i += batchSize) {
+    const batch = users.slice(i, i + batchSize);
+    await getDb().userRepository.save(batch);
+    console.log(`Seeding ${batch.length} users completed`);
+  }
+  console.log(`Seeding ${totalUser} users completed`);
+
   const laundromats: Laundromat[] = [];
-  for (let i = 0; i < totalLaundromat; i++) {
-    const laundromat = getDb().laundromatRepository.create({
-      name: fakerDE.company.name(),
-      street: fakerDE.location.streetAddress(),
-      city: fakerDE.location.city(),
-      country: fakerDE.location.country(),
-      postalCode: fakerDE.location.zipCode(),
-      price: Number(fakerDE.commerce.price()),
-      owner: users[Math.floor(Math.random() * users.length)],
-      lat: fakerDE.location.latitude().toString(),
-      lon: fakerDE.location.longitude().toString(),
-    });
-    laundromats.push(laundromat);
-  }
-  await getDb().laundromatRepository.save(laundromats);
+  users.forEach((user) => {
+    const totalLaundromat = randomNumber(1, 3);
+    for (let i = 0; i < totalLaundromat; i++) {
+      const laundromat = getDb().laundromatRepository.create({
+        name: fakerDE.company.name(),
+        street: fakerDE.location.streetAddress(),
+        city: fakerDE.location.city(),
+        country: fakerDE.location.country(),
+        postalCode: fakerDE.location.zipCode(),
+        price: Number(fakerDE.commerce.price()),
+        owner: user,
+        lat: fakerDE.location.latitude().toString(),
+        lon: fakerDE.location.longitude().toString(),
+      });
+      laundromats.push(laundromat);
+    }
+  });
 
-  // Make 5 random wash machines
-  const washingMachines: WashingMachine[] = [];
-  for (let i = 0; i < totalWashingMachine; i++) {
-    const washingMachine = getDb().washingMachineRepository.create({
-      name: fakerDE.company.name(),
-      brand: fakerDE.company.name(),
-      description: fakerDE.lorem.paragraph(),
-      laundromat: laundromats[Math.floor(Math.random() * laundromats.length)],
-    });
-    washingMachines.push(washingMachine);
+  console.log(`Seeding ${laundromats.length} laundromats...`);
+  for (let i = 0; i < laundromats.length; i += batchSize) {
+    const batch = laundromats.slice(i, i + batchSize);
+    await getDb().laundromatRepository.save(batch);
+    console.log(`Seeding ${batch.length} laundromats completed`);
   }
-  await getDb().washingMachineRepository.save(washingMachines);
+  console.log(`Seeding ${laundromats.length} laundromats completed`);
+
+  const washingMachines: WashingMachine[] = [];
+  laundromats.forEach((laundromat) => {
+    const totalWashingMachine = randomNumber(1, 5);
+    for (let i = 0; i < totalWashingMachine; i++) {
+      const washingMachine = getDb().washingMachineRepository.create({
+        name: fakerDE.company.name(),
+        brand: fakerDE.company.name(),
+        description: fakerDE.lorem.paragraph(),
+        laundromat,
+      });
+      washingMachines.push(washingMachine);
+    }
+  });
+
+  console.log(`Seeding ${washingMachines.length} washing machines...`);
+  for (let i = 0; i < washingMachines.length; i += batchSize) {
+    const batch = washingMachines.slice(i, i + batchSize);
+    await getDb().washingMachineRepository.save(batch);
+    console.log(`Seeding ${batch.length} washing machines completed`);
+  }
+  console.log(`Seeding ${washingMachines.length} washing machines completed`);
 };
 
 connectToDb()
