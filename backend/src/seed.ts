372dec4452d596c87ab4afc94cd24a1af56af7f8
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-02-03 18:04:07 +0100

    Refactor database seeding

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -1,10 +1,17 @@
 import { fakerDE } from '@faker-js/faker';
+import {
+  createUserWithEmailAndPassword,
+  deleteUser,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import getDb, { connectToDb } from './db';
+import Contract from './entities/contract';
 import Laundromat from './entities/laundromat';
 import User from './entities/user';
 import WashingMachine from './entities/washing-machine';
+import firebaseAuth from './utils/firebase';
 import './utils/load-env';
-import { randomNumber } from './utils/utils';
+import { randomDate, randomNumber } from './utils/utils';
 
 /**
  * Seed the database with random data
@@ -17,29 +24,70 @@ const seed = async () => {
 
   console.log('Seeding database...');
 
+  const startTime = Date.now();
+
   const batchSize = 1000;
 
-  const totalUser = randomNumber(100, 200);
-  const users: User[] = [];
+  const userMin = 100;
+  const userMax = 200;
+  const laundromatMinPerUser = 1;
+  const laundromatMaxPerUser = 3;
+  const washingMachineMinPerLaundromat = 1;
+  const washingMachineMaxPerLaundromat = 5;
+  const contractMinPerPseudoUser = 100;
+  const contractMaxPerPseudoUser = 200;
+
+  const totalUser = randomNumber(userMin, userMax);
+  const userMap = new Map<string, User>();
   for (let i = 0; i < totalUser; i++) {
     const user = getDb().userRepository.create({
       email: fakerDE.internet.email(),
       name: fakerDE.person.fullName(),
+      balance: 200,
     });
-    users.push(user);
+    userMap.set(user.email, user);
+
+    if (i === 0) {
+      try {
+        // If able to sign in then delete the user
+        const existingUserCredential = await signInWithEmailAndPassword(
+          firebaseAuth,
+          'seed@gmail.com',
+          'seed@gmail.com',
+        );
+
+        await deleteUser(existingUserCredential.user);
+      } catch (e) {
+        // Do nothing
+      }
+      const firebaseUser = await createUserWithEmailAndPassword(
+        firebaseAuth,
+        'seed@gmail.com',
+        'seed@gmail.com',
+      );
+      const user = getDb().userRepository.create({
+        id: firebaseUser.user.uid,
+        email: firebaseUser.user.email!,
+        name: 'Seed User',
+        balance: 200,
+      });
+      userMap.set(user.email, user);
+    }
   }
 
-  console.log(`Seeding ${totalUser} users...`);
-  for (let i = 0; i < totalUser; i += batchSize) {
+  const users = Array.from(userMap.values());
+
+  console.log(`Seeding ${users.length} users...`);
+  for (let i = 0; i < users.length; i += batchSize) {
     const batch = users.slice(i, i + batchSize);
     await getDb().userRepository.save(batch);
-    console.log(`Seeding ${batch.length} users completed`);
+    console.log(`Seeding batch ${i / 1000 + 1} users completed`);
   }
-  console.log(`Seeding ${totalUser} users completed`);
+  console.log(`Seeding ${users.length} users completed`);
 
   const laundromats: Laundromat[] = [];
   users.forEach((user) => {
-    const totalLaundromat = randomNumber(1, 3);
+    const totalLaundromat = randomNumber(laundromatMinPerUser, laundromatMaxPerUser);
     for (let i = 0; i < totalLaundromat; i++) {
       const laundromat = getDb().laundromatRepository.create({
         name: fakerDE.company.name(),
@@ -60,13 +108,16 @@ const seed = async () => {
   for (let i = 0; i < laundromats.length; i += batchSize) {
     const batch = laundromats.slice(i, i + batchSize);
     await getDb().laundromatRepository.save(batch);
-    console.log(`Seeding ${batch.length} laundromats completed`);
+    console.log(`Seeding batch ${i / 1000 + 1} laundromats completed`);
   }
   console.log(`Seeding ${laundromats.length} laundromats completed`);
 
   const washingMachines: WashingMachine[] = [];
   laundromats.forEach((laundromat) => {
-    const totalWashingMachine = randomNumber(1, 5);
+    const totalWashingMachine = randomNumber(
+      washingMachineMinPerLaundromat,
+      washingMachineMaxPerLaundromat,
+    );
     for (let i = 0; i < totalWashingMachine; i++) {
       const washingMachine = getDb().washingMachineRepository.create({
         name: fakerDE.company.name(),
@@ -82,9 +133,97 @@ const seed = async () => {
   for (let i = 0; i < washingMachines.length; i += batchSize) {
     const batch = washingMachines.slice(i, i + batchSize);
     await getDb().washingMachineRepository.save(batch);
-    console.log(`Seeding ${batch.length} washing machines completed`);
+    console.log(`Seeding batch ${i / 1000 + 1} washing machines completed`);
   }
   console.log(`Seeding ${washingMachines.length} washing machines completed`);
+
+  const threeMonthsAgo = new Date();
+  const threeMonthsAhead = new Date();
+  threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
+  threeMonthsAhead.setMonth(threeMonthsAhead.getMonth() + 3);
+  const contracts: Contract[] = [];
+  const totalContracts =
+    randomNumber(contractMinPerPseudoUser, contractMaxPerPseudoUser) * users.length;
+  for (let i = 0; i < totalContracts; i++) {
+    const user = users[randomNumber(0, users.length - 1)];
+    const washingMachine = washingMachines[randomNumber(0, washingMachines.length - 1)];
+    const startDate = randomDate(threeMonthsAgo, threeMonthsAhead);
+    const endDate = new Date(startDate);
+    endDate.setHours(endDate.getHours() + 2);
+    const contract = getDb().contractRepository.create({
+      startDate,
+      endDate,
+      price: washingMachine.laundromat.price,
+      user,
+      washingMachine,
+    });
+
+    // Check for overlapping contracts
+    if (
+      contracts.some(
+        (c) =>
+          c.washingMachine.id === contract.washingMachine.id &&
+          c.startDate < contract.endDate &&
+          c.endDate > contract.startDate,
+      )
+    ) {
+      continue;
+    }
+
+    contracts.push(contract);
+  }
+
+  console.log(`Seeding ${contracts.length} contracts...`);
+
+  for (let i = 0; i < contracts.length; i += batchSize) {
+    const batch = contracts.slice(i, i + batchSize);
+    await getDb().contractRepository.save(batch);
+    console.log(`Seeding batch ${i / 1000 + 1} contracts completed`);
+  }
+
+  console.log(`Seeding ${contracts.length} contracts completed`);
+
+  const balanceTransactions = contracts.flatMap((contract) => {
+    const senderBalance = getDb().balanceTransactionRepository.create({
+      name: `Contract #${contract.id}`,
+      amount: contract.price * -1,
+      user: contract.user,
+    });
+    const receiverBalance = getDb().balanceTransactionRepository.create({
+      name: `Contract #${contract.id}`,
+      amount: contract.price,
+      user: contract.washingMachine.laundromat.owner,
+    });
+    userMap.get(contract.user.email)!.balance -= contract.price;
+    userMap.get(contract.washingMachine.laundromat.owner.email)!.balance += contract.price;
+    return [senderBalance, receiverBalance];
+  });
+
+  console.log(`Seeding ${balanceTransactions.length} balance transactions...`);
+
+  for (let i = 0; i < balanceTransactions.length; i += batchSize) {
+    const batch = balanceTransactions.slice(i, i + batchSize);
+    await getDb().balanceTransactionRepository.save(batch);
+    console.log(`Seeding batch ${i / 1000 + 1} balance transactions completed`);
+  }
+
+  console.log(`Seeding ${balanceTransactions.length} balance transactions completed`);
+
+  const users2 = Array.from(userMap.values());
+
+  console.log('Updating users balance...');
+
+  for (let i = 0; i < users2.length; i += batchSize) {
+    const batch = users2.slice(i, i + batchSize);
+    await getDb().userRepository.save(batch);
+    console.log(`Updating balance batch ${i / 1000 + 1} completed`);
+  }
+
+  console.log('Updating users balance completed');
+
+  const endTime = Date.now();
+
+  console.log(`Database seeded in ${(endTime - startTime) / 1000} seconds`);
 };
 
 connectToDb()
