8161a687eeaf394e4e25c680d490fb11e10db653
Vinston Salim <vinston.salim@stud.h-da.de>
2024-02-06 19:11:03 +0100

    Refactor IndividualWashingMachineComponent to remove unused import and simplify code

--- frontend/src/components/ui/IndividualWashingMachineComponent.tsx
+++ frontend/src/components/ui/IndividualWashingMachineComponent.tsx
@@ -1,5 +1,4 @@
 import { Button, Badge, Card, Group, Text } from '@mantine/core';
-import Laundromat from '../../interfaces/entities/laundromat';
 import WashingMachine from '../../interfaces/entities/washing-machine';
 import { Individual } from '../../interfaces/ui/individual';
 
@@ -7,12 +6,11 @@ const IndividualWashingMachine: React.FC<Individual<WashingMachine>> = ({
   item: washingMachine,
   onItemClick,
 }: Individual<WashingMachine>) => {
-  const laundromat = washingMachine.laundromat as Laundromat;
   return (
     <Card shadow="sm" padding="md" m="md" radius="md" withBorder>
       <Group justify="space-between" mt="md" mb="xs">
         <Text fw={500}>Name: {washingMachine.name}</Text>
-        <Badge color="pink">{laundromat.price} €</Badge>
+        <Badge color="pink">{washingMachine.laundromat.price} €</Badge>
       </Group>
       <Text>Brand: {washingMachine.brand}</Text>
       <Text>Description: {washingMachine.description}</Text>
