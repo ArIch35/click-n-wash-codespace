bba008004be58dda97b57d7c74bcf2d5aed991af
Vinston Salim <vinston.salim@stud.h-da.de>
2024-02-06 19:00:15 +0100

    Add badge for price in IndividualWashingMachineComponent

--- frontend/src/components/ui/IndividualWashingMachineComponent.tsx
+++ frontend/src/components/ui/IndividualWashingMachineComponent.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, Text } from '@mantine/core';
+import { Button, Badge, Card, Group, Text } from '@mantine/core';
+import Laundromat from '../../interfaces/entities/laundromat';
 import WashingMachine from '../../interfaces/entities/washing-machine';
 import { Individual } from '../../interfaces/ui/individual';
 
@@ -6,9 +7,15 @@ const IndividualWashingMachine: React.FC<Individual<WashingMachine>> = ({
   item: washingMachine,
   onItemClick,
 }: Individual<WashingMachine>) => {
+  const laundromat = washingMachine.laundromat as Laundromat;
   return (
-    <Card>
-      <Text>{washingMachine.name}</Text>
+    <Card shadow="sm" padding="md" m="md" radius="md" withBorder>
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Name: {washingMachine.name}</Text>
+        <Badge color="pink">{laundromat.price} €</Badge>
+      </Group>
+      <Text>Brand: {washingMachine.brand}</Text>
+      <Text>Description: {washingMachine.description}</Text>
       <Button onClick={() => onItemClick!(washingMachine)}>Book</Button>
     </Card>
   );
