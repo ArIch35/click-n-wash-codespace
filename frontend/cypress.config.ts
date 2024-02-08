b32822fefcbc3aa0d6bb735bad1bb30390bf8f87
Vinsky Strauss TanHansen <vinsky.s.tanhansen@stud.h-da.de>
2024-02-08 16:01:20 +0100

    Update Cypress configuration to load environment variables

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import loadEnv from './cypressLoad-env';
 
 export default defineConfig({
+  env: { ...loadEnv() },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
