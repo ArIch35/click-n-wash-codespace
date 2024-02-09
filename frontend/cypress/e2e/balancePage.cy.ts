38e106670087a3eb584c163c5af26a10a4973704
Vinsky Strauss TanHansen <vinsky.s.tanhansen@stud.h-da.de>
2024-02-09 12:57:50 +0100

    Add balancePage.cy.ts with login, visit, add funds, and logout tests

---
+++ frontend/cypress/e2e/balancePage.cy.ts
@@ -0,0 +1,52 @@
+// Login function
+// Make a login function so that we can use it in the test
+function login() {
+  cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
+  cy.wait(1000);
+  cy.get('body').then((body) => {
+    if (body.find('button:contains("Login")').length > 0) {
+      cy.contains('button', 'Login').eq(0).should('exist').click();
+      cy.get('input[name="email"]').should('exist').type('newUser@mantine.de');
+      cy.get('input[name="password"]').should('exist').type('ValidP4$$w0rd');
+      cy.get('button[type="submit"]').eq(1).should('exist').click();
+    } else {
+      // If the button doesn't exist, return
+      return;
+    }
+  });
+}
+
+describe('visit the website', () => {
+  it('passes', () => {
+    // Use the .env with the help of load-env.ts
+    cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
+    cy.wait(1000);
+  });
+});
+
+describe('login', () => {
+  it('passes', () => {
+    login();
+  });
+});
+
+describe('Add Funds 20', () => {
+  it('passes', () => {
+    login();
+    cy.get('button').eq(0).should('exist').click();
+    cy.wait(1000);
+    cy.contains('a', 'Balance').should('exist').click();
+    cy.contains('button', 'Add Funds').should('exist').click();
+    cy.contains('button', '20').should('exist').click();
+    cy.contains('button', 'Topup').should('exist').click();
+  });
+});
+
+describe('log out from the website', () => {
+  it('passes', () => {
+    cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
+    cy.wait(1000);
+    cy.get('button').eq(2).should('exist').trigger('mouseover');
+    cy.contains('div', 'Logout').should('exist').click();
+  });
+});
