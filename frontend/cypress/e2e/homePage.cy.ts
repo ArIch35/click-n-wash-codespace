5f8a614e3cbf09ac43812ef26d654f4a34d2edf9
Vinsky Strauss TanHansen <vinsky.s.tanhansen@stud.h-da.de>
2024-02-09 11:58:55 +0100

    Refactor login and registration tests, add booking functionality

--- frontend/cypress/e2e/homePage.cy.ts
+++ frontend/cypress/e2e/homePage.cy.ts
@@ -1,34 +1,33 @@
+// Make a login function so that we can use it in the test
+function login() {
+  cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
+  cy.wait(1000);
+  cy.get('body').then((body) => {
+    if (body.find('button:contains("Login")').length > 0) {
+      cy.contains('button', 'Login').eq(0).should('exist').click();
+      cy.get('input[name="email"]').should('exist').type('newUser@mantine.de');
+      cy.get('input[name="password"]').should('exist').type('ValidP4$$w0rd');
+      cy.get('button[type="submit"]').eq(1).should('exist').click();
+    } else {
+      // If the button doesn't exist, return
+      return;
+    }
+  });
+}
+
+// Create new Date
+const date = new Date();
+const formattedDate = date.toLocaleDateString('en-GB', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+});
+
 describe('visit the website', () => {
   it('passes', () => {
     // Use the .env with the help of load-env.ts
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
     cy.wait(1000);
-  });
-});
-
-describe('check the login button', () => {
-  it('passes', () => {
-    cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
-    cy.get('[data-testid="login"]').should('exist');
-  });
-});
-
-describe('click the login modal', () => {
-  it('passes', () => {
-    cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
-    cy.get('[data-testid="login"]').click({ force: true });
-    cy.get('[data-testid="auth-modal"]').should('exist');
-  });
-});
-
-describe('click the register button', () => {
-  it('passes', () => {
-    cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
-    cy.get('[data-testid="login"]').click({ force: true });
-    cy.get('[data-testid="auth-modal"]').should('exist');
   });
 });
 
@@ -43,7 +42,21 @@ describe('register new user', () => {
     cy.get('input[name="password"]').should('exist').type('ValidP4$$w0rd');
     cy.get('input[name="confirmPassword"]').should('exist').type('ValidP4$$w0rd');
     cy.get('input[name="terms"]').should('exist').check();
-    cy.contains('button', 'Register').should('exist');
+    cy.contains('button', 'Register').should('exist').click();
+  });
+});
+
+describe('login', () => {
+  it('passes', () => {
+    login();
+  });
+});
+
+describe('click the hamburger button', () => {
+  it('passes', () => {
+    login();
+    cy.get('button').eq(0).should('exist').click();
+    cy.wait(1000);
   });
 });
 
@@ -52,19 +65,41 @@ describe('try to book', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
     cy.wait(1000);
     cy.contains('button', 'Show on map').should('exist').click();
+    cy.wait(1000);
   });
 
   it('should be able to book by clicking the image icon from map', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
     cy.wait(1000);
     cy.get('img[alt="Marker"]').eq(0).should('exist').click({ force: true });
-    cy.get('input[name="email"]').should('be.visible');
+    cy.wait(1000);
   });
 
   it('should be able to book by clicking the book button from list', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
     cy.wait(1000);
     cy.contains('button', 'Book').should('exist').click();
-    cy.get('input[name="email"]').should('be.visible');
+    cy.wait(1000);
+    cy.get(`button[aria-label="${formattedDate}"]`).should('exist').click();
+    cy.wait(1000);
+    cy.get('input').eq(4).should('exist').click();
+    cy.contains('div', /[0-9]{2}:00/)
+      .eq(0)
+      .should('exist')
+      .click();
+    cy.wait(1000);
+    cy.contains('button', 'Book Time').should('exist').click();
+    cy.wait(1000);
+    cy.get('ul div button').eq(0).should('exist').click({ force: true });
+    cy.wait(5000);
+  });
+});
+
+describe('log out from the website', () => {
+  it('passes', () => {
+    cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
+    cy.wait(1000);
+    cy.get('button').eq(2).should('exist').trigger('mouseover');
+    cy.contains('div', 'Logout').should('exist').click();
   });
 });
