3bcdd8a60666a6314f43f1651116b7b7c79c0925
stvitanh <vinsky.s.tanhansen@stud.h-da.de>
2024-02-12 15:56:18 +0100

    Update wait times in Cypress tests and add check inbox

--- frontend/cypress/e2e/homePage.cy.ts
+++ frontend/cypress/e2e/homePage.cy.ts
@@ -2,7 +2,7 @@
 // Make a login function so that we can use it in the test
 function loginHome() {
   cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-  cy.wait(1000);
+  cy.wait(3000);
   cy.get('body').then((body) => {
     if (body.find('button:contains("Login")').length > 0) {
       cy.contains('button', 'Login').eq(0).should('exist').click();
@@ -28,14 +28,14 @@ describe('visit the website', () => {
   it('passes', () => {
     // Use the .env with the help of load-env.ts
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
+    cy.wait(3000);
   });
 });
 
 describe('register new user', () => {
   it('passes', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
+    cy.wait(3000);
     cy.contains('button', 'Login').should('exist').click();
     cy.contains('button', 'Not a member yet? Register').should('exist').click();
     cy.get('input[name="name"]').eq(1).should('exist').type('newUser');
@@ -50,6 +50,9 @@ describe('register new user', () => {
 describe('login', () => {
   it('passes', () => {
     loginHome();
+    cy.wait(3000);
+    cy.get('div[role="alert"]').contains('You have been successfully logged in').should('exist');
+    cy.wait(3000);
   });
 });
 
@@ -57,50 +60,64 @@ describe('click the hamburger button', () => {
   it('passes', () => {
     loginHome();
     cy.get('button').eq(0).should('exist').click();
-    cy.wait(1000);
+    cy.wait(3000);
   });
 });
 
 describe('try to book', () => {
   it('should be able to click show on map of list washing machine', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
+    cy.wait(3000);
     cy.contains('button', 'Show on map').should('exist').click();
-    cy.wait(1000);
+    cy.wait(3000);
   });
 
   it('should be able to book by clicking the image icon from map', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
+    cy.wait(3000);
     cy.get('img[alt="Marker"]').eq(0).should('exist').click({ force: true });
-    cy.wait(1000);
+    cy.wait(3000);
   });
 
   it('should be able to book by clicking the book button from list', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
+    cy.wait(3000);
     cy.contains('button', 'Book').should('exist').click();
-    cy.wait(1000);
+    cy.wait(3000);
     cy.get(`button[aria-label="${formattedDate}"]`).should('exist').click();
-    cy.wait(1000);
+    cy.wait(3000);
     cy.get('input').eq(4).should('exist').click();
     cy.contains('div', /[0-9]{2}:00/)
       .eq(0)
       .should('exist')
       .click();
-    cy.wait(1000);
+    cy.wait(3000);
     cy.contains('button', 'Book Time').should('exist').click();
-    cy.wait(1000);
+    cy.wait(3000);
     cy.get('ul div button').eq(0).should('exist').click({ force: true });
-    cy.wait(1000);
+    cy.wait(3000);
+  });
+});
+
+describe('go to inbox', () => {
+  it('passes', () => {
+    cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
+    cy.wait(3000);
+    cy.get('button').eq(2).should('exist').trigger('mouseover');
+    cy.wait(3000);
+    cy.contains('button', 'Inbox').should('exist').click();
+    cy.wait(3000);
+    cy.contains('button', 'User Inbox').should('exist');
   });
 });
 
 describe('log out from the website', () => {
   it('passes', () => {
     cy.visit(Cypress.env('VITE_FRONTEND_ADDRESS') as string);
-    cy.wait(1000);
+    cy.wait(3000);
     cy.get('button').eq(2).should('exist').trigger('mouseover');
+    cy.wait(3000);
     cy.contains('div', 'Logout').should('exist').click();
+    cy.wait(3000);
   });
 });
