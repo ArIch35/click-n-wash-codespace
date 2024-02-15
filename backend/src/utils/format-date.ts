0cfdc4e2dc74d3c4cacbc3e210fab8a04bb18742
Agustinus Nicander Hery <nicanderhery@gmail.com>
2024-02-15 21:08:35 +0100

    Fix date formatting issue

--- backend/src/utils/format-date.ts
+++ backend/src/utils/format-date.ts
@@ -4,6 +4,10 @@
  * @returns Formatted date
  */
 const formatDate = (date: Date): string => {
+  // Convert to UTC, then to UTC+1
+  date = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
+  date = new Date(date.getTime() + 60 * 60 * 1000);
+
   const day = date.getDate().toString().padStart(2, '0');
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear();
