2e5493666c6eb4b92ec3cd7af2ca30ee5a5048d2
Vinston Salim <vinston.salim@stud.h-da.de>
2024-02-17 23:10:55 +0100

    Add missing page layout and content

--- frontend/src/pages/MissingPage.tsx
+++ frontend/src/pages/MissingPage.tsx
@@ -1,7 +1,34 @@
-import { Text } from '@mantine/core';
+import { Center, Image, Title, Text, Button, Container, Grid } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
 
 const MissingPage = () => {
-  return <Text>Missing page</Text>;
+  const navigate = useNavigate();
+
+  return (
+    <Container p={1}>
+      <Center>
+        <Grid>
+          <Grid.Col>
+            {' '}
+            <Image
+              src="https://ui.mantine.dev/_next/static/media/image.11cd6c19.svg"
+              alt="404 Image"
+            />
+          </Grid.Col>
+          <Grid.Col>
+            <Title>Sorry, we can&apos;t find that page</Title>
+            <Text size="lg">
+              Uh-oh! The page you&apos;re hunting for seems to have gone on a digital scavenger
+              hunt. Double-check the address, or contact support if you suspect some mischief afoot!
+            </Text>
+            <Button variant="outline" size="md" mt="xl" onClick={() => navigate('/')}>
+              Get back to home page
+            </Button>
+          </Grid.Col>
+        </Grid>
+      </Center>
+    </Container>
+  );
 };
 
 export default MissingPage;
