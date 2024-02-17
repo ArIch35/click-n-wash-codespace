743cadb516647f7682a34a32407ed409a8185769
Vinston Salim <vinston.salim@stud.h-da.de>
2024-02-17 23:11:14 +0100

    Add EmptyData component

---
+++ frontend/src/components/EmptyData.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { IconReceiptOff } from '@tabler/icons-react';
+import { Card, Container, Flex, Text } from '@mantine/core';
+
+interface EmptyDataProps {
+  message: string;
+}
+
+const EmptyData: React.FC<EmptyDataProps> = ({ message }) => {
+  return (
+    <Container>
+      <Card shadow="sm" padding="lg" radius="lg" style={{ width: 400 }}>
+        <Flex direction="column" align="center">
+          <IconReceiptOff size={30} />
+          <Text>No Entry For {message} Found</Text>
+        </Flex>
+      </Card>
+    </Container>
+  );
+};
+
+export default EmptyData;
